91d5df02913d8d07cfb616bf2e9d74ecfb4dbca1
huanglizhe <32793498@qq.com>
2016-01-15 20:52:36 +0800

    修改了 systemUtil格式

--- src/com/u4f/tools/SystemUtil.java
+++ src/com/u4f/tools/SystemUtil.java
@@ -2,31 +2,39 @@ package com.u4f.tools;
 
 public class SystemUtil
 {
-	static final String systemVersion="Mac OS X";
+	private static final String systemVersion = "Mac OS X";
+	private static final String MAC_MYSQL_PASSWORD = "";
+	private static final String WINDOWS_MYSQL_PASSWORD = "hourui";
+
+	public static void main(String a[])
+	{
+		System.out.println(getUploadPah());
+	}
+	public static String getUploadPah()
+	{
+		String path = "";
+		if (isMac())
+		{
+			path = "/Users/hourui/Workspaces/MyEclipse 10/u4f/WebRoot/uploadImg/";
+		} else
+			path = "E:/MyEclipse/u4f/WebRoot/uploadImg/";
+		return path;
+	}
+
+	public static boolean isMac()
+	{
+		String systemName = System.getProperties().getProperty("os.name");
+		if (systemVersion.equals(systemName))
+		{
+			return true;
+		} else
+			return false;
+	}
 	
-   public static String getUploadPah(){
-	   String path="";	  
-	   if( isMac()){
-		   path="/Users/hourui/Workspaces/MyEclipse 10/u4f/WebRoot/uploadImg/";
-	   }else
-		   path="E:/MyEclipse/u4f/WebRoot/uploadImg/";
-	   return path;
-   }
-   
-   public static String getMySqlPwd(){
-	   String pwd="";
-	   if(!isMac()){
-		   pwd="hourui";
-	   }
-	   return pwd;
-   }
-   
-   public static boolean isMac(){	   
-	   String s=System.getProperties().getProperty("os.name");
-	   if(systemVersion.equals(s)){
-		   return true;
-	   }
-	   else return false;	   
-   }
-    
+	public static String getMySqlPwd()
+	{
+		String pwd =isMac() == true ? MAC_MYSQL_PASSWORD :WINDOWS_MYSQL_PASSWORD;
+		return pwd;
+	}
+
 }
